8d168d27392b7ce0a744bb50d0ef7ae4adc823e0
Obinna <obikelv@gmail.com>
2024-03-28 13:06:57 +0100

    userModel file correction

--- models/userModel.js
+++ models/userModel.js
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema({
         unique: [true, "Email address already registered"],
     },
     password: {
-        type: Strin,
+        type: String,
         required: [true, "Please add the user password"],
     },
 },
